423fe7054572f458e0f7400836b13c56bfa2d21c
HoangPham <hoapha1009@gmail.com>
2020-11-27 16:05:27 +0700

    Finish Hero section

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
+import { Link } from 'react-router-dom';
+import Button from '../Button';
 
 HeroSection.propTypes = {};
 
@@ -24,7 +26,53 @@ function HeroSection({
                 }
             >
                 <div className="container">
-                    <div className="row home__hero-row"></div>
+                    <div
+                        className="row home__hero-row"
+                        style={{
+                            display: 'flex',
+                            flexDirection:
+                                imgStart === 'start' ? 'row-reverse' : 'row',
+                        }}
+                    >
+                        <div className="col">
+                            <div className="home__hero-text-wrapper">
+                                <div className="top-line">{topLine}</div>
+                                <h1
+                                    className={
+                                        lightText ? 'heading' : 'heading dark'
+                                    }
+                                >
+                                    {headline}
+                                </h1>
+                                <p
+                                    className={
+                                        lightTextDesc
+                                            ? 'home__hero-subtitle'
+                                            : 'home__hero-subtitle dark'
+                                    }
+                                >
+                                    {description}
+                                </p>
+                                <Link to="/sign-up">
+                                    <Button
+                                        buttonSize="btn--wide"
+                                        buttonColor="blue"
+                                    >
+                                        {buttonLabel}
+                                    </Button>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="home__hero-img-wrapper">
+                                <img
+                                    src={img}
+                                    alt={alt}
+                                    className="home__hero-img"
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
