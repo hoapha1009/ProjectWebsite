be5c0824ab072cc1d0df695f5c715e778f9f52bc
HoangPham <hoapha1009@gmail.com>
2020-11-28 09:27:10 +0700

    Set up form

---
+++ src/features/Auth/RegisterForm/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TextField } from '@material-ui/core';
+
+RegisterForm.propTypes = {};
+
+function RegisterForm(props) {
+    return (
+        <form>
+            <TextField />
+        </form>
+    );
+}
+
+export default RegisterForm;
