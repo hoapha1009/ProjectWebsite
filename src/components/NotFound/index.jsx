bc8f20b21fbe792820b2c9e05afc2ab9a474d465
HoangPham <hoapha1009@gmail.com>
2020-11-29 21:41:39 +0700

    update not found component

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.scss';
 
 NotFound.propTypes = {};
 
 function NotFound(props) {
-    return <div>NOT FOUND ...</div>;
+    return (
+        <div class="mainbox">
+            <div class="err">4</div>
+            <i class="far fa-question-circle fa-spin"></i>
+            <div class="err2">4</div>
+            <div class="msg">
+                Maybe this page moved? Got deleted? Is hiding out in quarantine?
+                Never existed in the first place?
+                <p>
+                    Let's go <Link to="/">home</Link> and try from there.
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default NotFound;
